10cb1a560540f008e0480f3d6d28165d852937cc
Abhinav <abhinavbhardwaj898@gmail.com>
2025-03-31 18:15:21 +0530

    Primary annotation usage

---
+++ use_of_primary_annotation/src/main/java/com/abhi/it/ReportService.java
@@ -0,0 +1,17 @@
+package com.abhi.it;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportService {
+	
+	@Autowired
+	ReportDao reportDao;
+	
+	public void reportProcessing() {
+		System.out.println("Report is generating....");
+		reportDao.generateReport();
+	}
+}
