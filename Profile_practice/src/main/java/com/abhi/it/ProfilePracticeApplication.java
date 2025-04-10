54c015cce7a0343819bbf7049df3d3a1dee50e97
Abhinav <abhinavbhardwaj898@gmail.com>
2025-04-10 11:45:33 +0530

    Profiles practice

---
+++ Profile_practice/src/main/java/com/abhi/it/ProfilePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.abhi.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilePracticeApplication.class, args);
+	}
+
+}
