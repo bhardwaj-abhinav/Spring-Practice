6c2f4320aebc52c85d0574a474b4abc78d332f21
Abhinav <abhinavbhardwaj898@gmail.com>
2025-04-14 16:04:04 +0530

    Added more validation

--- Product_project_using_validarion_and_thymeleaf/src/main/java/com/abhi/it/entity/Product.java
+++ Product_project_using_validarion_and_thymeleaf/src/main/java/com/abhi/it/entity/Product.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -21,9 +22,11 @@ public class Product {
 	private String name;
 	
 	@NotNull(message = "Price is mandatory")
+	@Positive(message = "Price must be positive")
 	private Integer price;
 	
 	@NotNull(message = "Quantity is mandatory")
+	@Positive(message = "Price must be positive")
 	private Integer qty;
 	
 }
