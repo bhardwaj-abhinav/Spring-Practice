44943f1c41eaf99227543800b54ed769d34652a3
Abhinav <abhinavbhardwaj898@gmail.com>
2025-04-15 12:50:42 +0530

    uses of response body annotation

---
+++ ResponseBody_Annotation_uses/src/main/java/com/abhi/it/controller/DemoCOntroller.java
@@ -0,0 +1,24 @@
+package com.abhi.it.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoCOntroller {
+	
+	@GetMapping("/")
+	public String getWelcome(Model model) {
+		String msg = "Hi All, Welcome....";
+		model.addAttribute("msg" ,msg);
+		return "index";
+	}
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String getGreet() {
+		String msg = "Good Morning";
+		return msg;
+	}
+}
